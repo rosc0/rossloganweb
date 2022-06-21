9f1e3e757795b46c023380e212428922ae56b713
Ross Logan <ross.j.logan@gmail.com>
2022-06-21 12:53:25 +1200

    Added custon tailwind colours, and fixed up nav

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,35 @@
 import { useState, useEffect } from 'react'
 import mePic from '../assets/me.jpg'
 
-import Title from './Title';
+import Title from './Title'
 
 interface HighlightProps {
-  text: string,
+  text: string
 }
 
-const Highlight = ( { text } : HighlightProps ) => {
+const Highlight = ({ text }: HighlightProps) => {
   return <span className='text-sky-500'>{text}</span>
 }
 
 function About() {
+  const [years, setYears] = useState<number | null>(null)
 
-  const [years, setYears] =  useState<number | null>(null);
-
-  const getYears = (): number => { 
-    const d = new Date();
-    const thisYear = d.getFullYear();
-    const startYear = 2008;
-    const careerBreak = 3;
-    const years = thisYear - startYear - careerBreak;
+  const getYears = (): number => {
+    const d = new Date()
+    const thisYear = d.getFullYear()
+    const startYear = 2008
+    const careerBreak = 3
+    const years = thisYear - startYear - careerBreak
     return years
   }
 
   useEffect(() => {
     setYears(getYears())
   }, [])
+
+  const sendEmail = (): void => {
+    alert('TODO: Send email')
+  }
 
   return (
     <>
@@ -36,32 +39,31 @@ function About() {
           <img
             src={mePic}
             alt='Ross Logan'
-            className='m-auto w-2/3 rounded-full lg:w-full shadow-2xl'
+            className='m-auto w-2/3 rounded-full lg:w-full shadow-2xl shadow-gray-700'
           />
         </div>
         <p className='m-auto pl-0 pt-12 w-full text-gray-400 text-justify text-xl lg:pt-0 lg:pl-16 lg:w-2/3 xl:w-3/4'>
           My name is <Highlight text='Ross Logan' /> and I am a <Highlight text='frontend web developer' />.
           <br />
           <br />
-          I started developing websites in 2008, giving me about <Highlight text={`${years} years experience`} />. 
-          I have worked for different companies here in New Zealand and in Amsterdam where I lived for 6 years.
+          I started developing websites in 2008, giving me about <Highlight text={`${years} years experience`} />. I have worked for
+          different companies here in New Zealand and in Amsterdam where I lived
+          for 6 years.
           <br />
           <br />
-          Over the years I have learnt a lot of different web technologies, but my passion is with Javascript, React and React Native.
+          Over the years I have learnt a lot of different web technologies, but
+          my passion is with <Highlight text='JavaScript' />, <Highlight text='React' /> and <Highlight text='React Native' />.
           <br />
           <br />
-          I'm currently looking for any kind of web-related work that's
-          out there. I have experience with with a range of technologies and I
-          am not afraid to learn something new to help you. 
+          I have experience with with a range of technologies and I am not
+          afraid to learn something new to help you.
           <br />
           <br />
-          Send me an email so
-          we can look at how I can help you with any projects you might have.
+          <button className='text-white underline underline-offset-2' onClick={sendEmail}>Send me an email</button> so we can look at how I can help you with any projects you might have.
         </p>
       </div>
     </>
   )
-
 }
 
 export default About
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,15 +8,29 @@ interface NavProps {
 
 function Nav({ scrolledToNav }: NavProps) {
   return (
-    <div id='navBar' className={`w-full mt-12 border-opacity-0 border-b-2 border-sky-500 lg:mt-24 ${scrolledToNav ? 'fixedToTop' : ''}`}>
-      
+    <div
+      id='navBar'
+      className={`w-full mt-12 bg-charcoal-light border-opacity-0 border-b-2 border-sky-500 lg:mt-24 ${
+        scrolledToNav
+          ? 'fixedToTop opacity-100 bg-charcoal-light border-opacity-20 transition-all ease-in-out duration-200'
+          : ''
+      }`}
+    >
       <div className='container mx-auto flex'>
-
-        <div id='navLogo' className={`m-auto opacity-0 pl-6 ${scrolledToNav ? 'w-4/12' : 'w-0'}`}>
+        <div
+          id='navLogo'
+          className={`m-auto opacity-0 pl-6 ${
+            scrolledToNav ? 'w-4/12' : 'w-0'
+          }`}
+        >
           <LogoSVG title='Ross Logan Web' />
         </div>
 
-        <div className={`pr-6 ${scrolledToNav ? 'w-8/12 text-right' : 'w-full text-center'}`}>
+        <div
+          className={`pr-6 ${
+            scrolledToNav ? 'w-8/12 text-right' : 'w-full text-center'
+          }`}
+        >
           <ul className='inline-block text-white'>
             <li className='inline-block py-6 px-3 lg:px-6'>About</li>
             <li className='inline-block py-6 px-3 lg:px-6'>Capabilities</li>
@@ -24,9 +38,7 @@ function Nav({ scrolledToNav }: NavProps) {
             <li className='inline-block py-6 pl-3 lg:pl-6'>Contact</li>
           </ul>
         </div>
-
       </div>
-
     </div>
   )
 }
